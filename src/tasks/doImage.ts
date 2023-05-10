0cc6d901f2e55e5bc1db12de59e0388bcedf4173
dcsan <dc@rik.ai>
2023-05-10 02:09:10 +0100

    images posting

--- src/tasks/doImage.ts
+++ src/tasks/doImage.ts
@@ -19,27 +19,15 @@ const config = {
 export async function doImage(opts: PostParams) {
   const { agent } = opts;
 
+  // const imagePath = './testdata/test-cat.jpg'
   // const imagePath = './testdata/slice.png'
-  const imagePath = './testdata/test-cat.jpg'
-  // const imagePath = './testdata/test.png'
+  const imagePath = './testdata/test.png'
 
   const params: MakeEmbedParams = {
     agent,
     imageUrl: imagePath
   }
   const embed = await makeEmbed(params)
-
-  // try {
-  //   const result = await bot.post({
-  //     text: "here's an image",
-  //     embed,
-  //   });
-  //   console.log('posted image', result)
-  // } catch (err) {
-  //   console.error('error posting image', err)
-  // }
-
-
   const text = '[test] image post';
 
   const response = await agent.post({
